bfc5b87ea79644b5f18fd80f265ca93fe05bb693
marvewon <marvelouswon4@yahoo.com>
2022-09-14 11:46:26 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 
 /**
-* main - prints sum of even fibonacci
-* suit elements
-* fibonacci suit numbers
-* Return: return 0
+ * main – function to finds and prints the first 98 Fibonacci numbers,
+ *
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
 */
 int main(void)
 {
-unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
-while (n3 <= 4000000)
-{
-n3 = n1 + n2;
-n1 = n2;
-n2 = n3;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-if ((n1 % 2) == 0)
-sum += n1;
-
-}
-printf("%ld\n", sum);
-return (0);
+	j = 1;
+	k = 2;
+	printf(" %lu ", j);
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu ", k);
+		k = k + j;
+		j = k – j;
+	}
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu ", k1 + (k2 / 1000000000));
+		printf(" %lu ", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 – j1;
+		k2 = k2 + j2;
+		j2 = k2 – j2;
+	}
+	printf("\n");
+	return (0);
 }
