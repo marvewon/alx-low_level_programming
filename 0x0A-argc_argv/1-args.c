10b11544af166d2ecf46f75860e57b52aef3c578
marvewon <marvelouswon4@yahoo.com>
2022-10-05 11:19:44 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed into it
+ * @argc: number of arguments passed into the function
+ * @arcv: number of arguments vectors to the pointers
+ *
+ * Return: Always 0
+ */
+int main(int argc, char * argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc-1);
+	return (0);
+}
+		
