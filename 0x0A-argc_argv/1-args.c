77d79a565a35d7816cec9a36eacf25530721448c
marvewon <marvelouswon4@yahoo.com>
2022-10-05 11:27:39 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed into it
+ * main - prints the number of arguments passed into it
  * @argc: number of arguments passed into the function
- * @arcv: number of arguments vectors to the pointers
+ * @argv: argument vector of pointers to strings
  *
- * Return: Always 0
+ * Return: always 0
  */
-int main(int argc, char * argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc-1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
-		
