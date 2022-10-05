d052478a3e0e3aba7a8c931336e695b4dea05e58
marvewon <marvelouswon4@yahoo.com>
2022-10-05 10:56:43 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - call all functions
+ *
+ * Return: 0 Always
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
