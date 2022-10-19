8c958f8a4355bc9b2d17287d29dcf5865548e87d
marvewon <marvelouswon4@yahoo.com>
2022-10-19 15:32:22 -0700

    doen

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,8 @@
+/**
+ * File: 0-is_prime_number.c
+ * Auth: Marvelous Won
+ */
+
 #include "main.h"
 
 int is_divisible(int num, int div);
@@ -5,12 +10,14 @@ int is_prime_number(int n);
 
 /**
  * is_prime_number - function that returns 1 if the input is a prime no
- * @n: integer
+ * @num: number to be checked
+ * @div: the divisor
  *
- * Return: return 1 if n is a prime no, else return 0
+ * Return: if num is divisible by 0
+ * 	   if num is not divisible is return - 1
  */
 int is_divisible(int num, int div)
 {
 	if (num % div == 0)
 		return (0);
-
+}
