1161f09bb72ff20807eb6680682014d1ebf0f27a
marvewon <marvelouswon4@yahoo.com>
2022-11-10 12:52:13 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - call all function
+ * main - program that prints its name, followed by a new line
  *
+ * @argc: argument count
+ * @argv: argument vector
+ * 
  * Return: Always 0
  */
 int main (int argc, char *argv[])
 {
-	int i;
+	(void)argc;
 
-	if (argc > 1)
-	{
-		for (i = 0; i < argc; i ++)
-		{
-			printf("argc[%d] = %s\n", i, argv[i]);
-		}
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
