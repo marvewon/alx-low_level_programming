42863f8d69cebf2a2d4b05bcf724746363b0be78
marvewon <marvelouswon4@yahoo.com>
2022-11-10 14:08:44 -0800

    taskt 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,11 +13,15 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc == 1)
+	{
 		printf("%d\n", argc - 1);
+	}
 	else
 	{
-		for (i = 0; *argv; i++, argv++);
-		printf("%d\n", i - 1);
+		for (i = 0; *argv; i++, argv++)
+		{
+			printf("%d\n", i - 1);
+		}
 	}
 	return (0);
 }
