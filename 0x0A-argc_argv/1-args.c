c24e3ed40a367ce2d5ef253f18ee329bb21e142e
marvewon <marvelouswon4@yahoo.com>
2022-11-11 01:24:37 -0800

    fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -15,13 +15,9 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("%d\n", argc - 1);
-	}
 	else
-	{
-		for (i = 0; *argv; i++, argv++)
-		{
+		for (i = 0; *argv; i++, argv++);
 			printf("%d\n", i - 1);
-		}
 	}
 	return (0);
 }
